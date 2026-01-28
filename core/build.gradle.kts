b2f8d066f34330f1b6080a248c4ab00357527b57
range79 <darkrange006@gmail.com>
2026-01-28 15:43:05 +0400

    Meili task checker endpoint updated and integraion test added

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation("com.google.guava:guava:32.1.2-jre")
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
+    // Source: https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    compileOnly("org.slf4j:slf4j-api:2.0.17")
 }
 
 tasks.test {
